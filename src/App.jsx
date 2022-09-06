cff01db5d636e56358b7ce879ce88b1baade4cff
Adelani <harounadelani@gmail.com>
2022-09-06 19:29:30 +0100

    almost done now

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import { useEffect } from 'react'
 import React from 'react'
 import {nanoid} from 'nanoid'
 import Quiz from "./quiz"
-// import "./index.css"
 
 let correctAnswersCount = 0
 
 function App() {
-  const [quizData, setQuizData] = useState([])
+
+  const initialData = () => {
+        let loading = []
+        for (let n = 0; n < 5; n ++) {
+            loading.push({
+                id: Math.ceil(Math.random() * 999999999),
+                q: "Loading...",
+                correctAns: "...",
+                randomAnswers: [". . .",". . .",". . .", ". . ."],
+                chosen_answer: "none",
+                isAnswerSelected: new Array(4).fill(false),
+                answersIds: new Array(4).fill(null).map(item => nanoid())
+            })
+        }
+        return loading
+    }
+  const [quizData, setQuizData] = useState(initialData)
   const [startGame, setStartGame] = useState(false)
   const [gameDone, setGameDone] = useState(false)
   const [score, setScore] = useState(0)
   const [restart, setRestart] = useState(false)
-  const [isSelected, setIsSelected] = useState(false)
+  const [correction, setCorrection] = useState(false)
 
   function handleClick(){
     setStartGame(prev => !prev)
@@ -32,25 +47,27 @@ function App() {
           q: item.question,
           correctAns: item.correct_answer,
           randomAnswers: shuffleAnswers([...item.incorrect_answers, item.correct_answer]),
-          chosen_answer: ""
+          chosen_answer: "",
+          isAnswerSelected: new Array(item.incorrect_answers.length + 1).fill(false),
+          answersIds: new Array(item.incorrect_answers.length+1).fill(null).map(item => nanoid())
         }
       }))
     })
   }, [restart])
 
   function chooseAnswer (event){
-    setIsSelected(prev => !prev)
-    setQuizData(prev => prev.map(quest => {
-        if(event.target.id == quest.id){
-          // console.log(event.target.selected)
-          return {...quest, chosen_answer: event.target.value}
-        }
-        else{
-          return quest
-        }
-      })
-      
-  )
+    setQuizData(prev => JSON.parse(JSON.stringify(prev)).map(quest => {
+              if(event.target.id == quest.id ){
+              quest.chosen_answer = event.target.value
+                  quest.randomAnswers.forEach((answer, index) => {
+                    answer === event.target.value ? quest.isAnswerSelected[index] = true 
+                    : quest.isAnswerSelected[index] = false
+                  })
+                  
+              }
+              return quest
+            })
+        )
 }
 
 function checkAnswers (){
@@ -60,6 +77,15 @@ function checkAnswers (){
          setScore(prev => prev + 1)
       }
     }
+
+    setCorrection (prev => !prev)
+    quizData.forEach((item)=> {
+      if (correction ){
+          if(item.correctAns === item.chosen_answer && item.isAnswerSelected) {
+            console.log("yes")
+          }
+      }
+    })
   }
 
 function restartGame (){
@@ -79,14 +105,15 @@ function restartGame (){
   const quizElements = quizData.map(item => {
     return < Quiz  
                 key={item.id}
-                isSelected={isSelected}
+                isSelected={item.selectedStatus}
+                answersIds={item.answersIds}
                 dataId={item.id}
+                isAnswerSelected={item.isAnswerSelected}
                 question={item.q}
                 chosenAnswer={item.chosen_answer}
                 chooseAnswer={chooseAnswer}
                 correctAnswer={item.correctAns}
                 options={item.randomAnswers}
-                
           />
   })
 
@@ -102,7 +129,8 @@ function restartGame (){
           {startGame && <main className="py-2">
           {quizElements}
           <div className="flex justify-center items-center space-x-3 mt-12">
-            {gameDone && <h1 className="font-bold text-lg text-[#293264]">You scored {score}/5 correct answers</h1>}
+            {gameDone && <h1 className="font-bold md:text-lg text-[#293264]">
+              You got {score}/5 correct answers!{score === 3 ? "👍🏻" : score > 3 ? "🥳" : "😬"}</h1>}
             {!gameDone && <button onClick={checkAnswers} className="w-[30%] py-2 bg-[#293264] text-white rounded-lg hover:bg-sky-400 tracking-wider font-bold">Check Answers</button>}
 
 
@@ -110,9 +138,9 @@ function restartGame (){
           </div>
           </main>}
           
-          {!startGame && <div className="p-2 text-[#293264] text-center space-y-4 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+          {!startGame && <div className="p-2 text-[#293264] text-center space-y-6 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
               <h1 className="md:text-4xl text-2xl font-bold">QUIZZICAL</h1>
-              <p className="tracking-tighter md:tracking-widest">Unleash the genious within! 🤓</p>
+              <p className="tracking-tighter md:tracking-widest"> ❔❓  What've you got ❔❓</p>
               <button onClick={handleClick} className="px-6 py-2 bg-[#293264] text-white rounded-lg hover:bg-sky-400 tracking-wider font-bold">Start quiz</button>
           </div>}
 
--- src/quiz.jsx
+++ src/quiz.jsx
@@ -3,44 +3,23 @@ import "./index.css"
 
 export default function Quiz (props){
 
+
+    const quizOptions = props.options.map((option, index)=> {
+        return <button 
+                    key={props.answersIds[index]}
+                    id={props.dataId}
+                    value={option} 
+                    onClick={props.chooseAnswer}
+                    isselected={props.isAnswerSelected[index].toString()}
+                    className="answer py-1 border-2 rounded-lg leading-tight text-left pl-1 tracking-tighter text-[10px] md:text-sm hover:bg-[#D6DBF5] ">{option}
+                </button>
+    })
+
     return (
         <div className="flex flex-col text-[#293264] mt-8 space-y-3 border-b-2 w-full">
             <p className="md:tracking-widest md:text-lg tracking-wider font-bold leading-tight">{props.question}</p>
             <div className="grid grid-cols-4 gap-3 w-full pb-4">
-                <button 
-                    id={props.dataId}
-                    value={props.options[0]} 
-                    onClick={props.chooseAnswer}
-                    selected = {props.isSelected}
-                    className="btn py-1 border-2 rounded-lg leading-tight text-left pl-1 tracking-tighter text-[10px] md:text-sm hover:bg-[#D6DBF5] ">{props.options[0]}
-                </button>
-
-                <button 
-                    id={props.dataId}
-                    value={props.options[1]} 
-                    onClick={props.chooseAnswer}
-                    selected = {props.isSelected} 
-                    className="btn py-1 border-2 rounded-lg leading-tight text-left pl-1 tracking-tighter text-[10px] md:text-sm  hover:bg-[#D6DBF5] ">{props.options[1]}
-                </button>
-
-
-                <button 
-                    id={props.dataId}
-                    value={props.options[2]} 
-                    onClick={props.chooseAnswer}
-                    selected = {props.isSelected}
-                    className="btn py-1 border-2 rounded-lg leading-tight text-left pl-1 tracking-tighter text-[10px] md:text-sm hover:bg-[#D6DBF5] ">{props.options[2]}
-                </button>
-
-
-                <button 
-                    id={props.dataId} 
-                    value={props.options[3]}
-                    onClick={props.chooseAnswer} 
-                    selected = {props.isSelected}
-                    className="btn py-1 border-2 rounded-lg leading-tight tracking-tighter text-left pl-1 md:text-sm text-[10px]  hover:bg-[#D6DBF5] ">{props.options[3]}
-                </button>
-                
+                {quizOptions}
             </div>
         </div>
     )
